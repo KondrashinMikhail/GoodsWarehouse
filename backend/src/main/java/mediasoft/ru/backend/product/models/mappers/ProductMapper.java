56b001fb6b1d1fb605647f712780f1941f6094aa
KondrashinMikhail <kondrashin.mihail@mail.ru>
2024-04-25 11:54:15 +0400

    Удаление ненужного кода

--- backend/src/main/java/mediasoft/ru/backend/product/models/mappers/ProductMapper.java
+++ backend/src/main/java/mediasoft/ru/backend/product/models/mappers/ProductMapper.java
@@ -2,7 +2,6 @@ package mediasoft.ru.backend.product.models.mappers;
 
 import mediasoft.ru.backend.product.models.dto.CreateProductDTO;
 import mediasoft.ru.backend.product.models.dto.ProductDTO;
-import mediasoft.ru.backend.product.models.dto.UpdateProductDTO;
 import mediasoft.ru.backend.product.models.entities.Product;
 import org.mapstruct.Mapper;
 
@@ -11,6 +10,4 @@ public interface ProductMapper {
     ProductDTO mapToDTO(Product product);
 
     Product mapToModel(CreateProductDTO createProductDTO);
-
-    UpdateProductDTO mapToDTO(ProductDTO productDTO);
 }
