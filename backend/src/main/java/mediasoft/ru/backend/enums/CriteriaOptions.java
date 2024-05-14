54aded161e4cef547c240ceb3324f9c003452f8f
KondrashinMikhail <90245664+KondrashinMikhail@users.noreply.github.com>
2024-05-14 09:00:32 +0400

    Обновление тестов (#8)

--- backend/src/main/java/mediasoft/ru/backend/enums/CriteriaOptions.java
+++ backend/src/main/java/mediasoft/ru/backend/enums/CriteriaOptions.java
@@ -1,4 +1,4 @@
-package mediasoft.ru.backend.criteria;
+package mediasoft.ru.backend.enums;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
