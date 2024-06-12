193c5facdc60da6f9b437a1973ab949f0fddafb2
KondrashinMikhail <kondrashin.mihail@mail.ru>
2024-06-12 10:35:54 +0400

    Добавление kafka в проект

---
+++ backend/src/main/java/mediasoft/ru/backend/kafka/CustomKafkaListener.java
@@ -0,0 +1,34 @@
+package mediasoft.ru.backend.kafka;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import mediasoft.ru.backend.exceptions.NotImplementedException;
+import mediasoft.ru.backend.kafka.handlers.KafkaEvent;
+import mediasoft.ru.backend.kafka.handlers.KafkaEventHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+@AllArgsConstructor
+public class CustomKafkaListener {
+    private Set<KafkaEventHandler<KafkaEvent>> eventHandlers;
+
+    @KafkaListener(topics = "${spring.kafka.template.default-topic}", groupId = "${spring.kafka.consumer.group-id}")
+    public void listen(String message) {
+        try {
+            final ObjectMapper objectMapper = new ObjectMapper();
+            final KafkaEvent eventSource = objectMapper.readValue(message, KafkaEvent.class);
+
+            eventHandlers.stream()
+                    .filter(eventSourceEventHandler -> eventSourceEventHandler.canHandle(eventSource))
+                    .findFirst()
+                    .orElseThrow(() -> new NotImplementedException("Handler for eventsource not found"))
+                    .handleEvent(eventSource);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ backend/src/main/java/mediasoft/ru/backend/models/dto/kafka/UpdateOrderStatusKafkaEventDTO.java
@@ -0,0 +1,30 @@
+package mediasoft.ru.backend.models.dto.kafka;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import mediasoft.ru.backend.enums.KafkaEventSource;
+import mediasoft.ru.backend.enums.OrderStatus;
+import mediasoft.ru.backend.kafka.handlers.KafkaEvent;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateOrderStatusKafkaEventDTO implements KafkaEvent {
+    private UUID customerId;
+    private UUID orderId;
+    private OrderStatus status;
+
+    @Override
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    public KafkaEventSource getEvent() {
+        return KafkaEventSource.UPDATE_ORDER_STATUS;
+    }
+}
